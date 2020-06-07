83dfd0c8b9971b1d6f330dfa2f6b135037398606
Alejandro González <thytanium@gmail.com>
2020-06-07 19:31:02 -0300

    Setup eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2020': true,
+    'jest/globals': true,
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:jest/recommended',
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 11,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react',
+    'react-hooks',
+    'jsx-a11y',
+    'jest',
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+  },
+  'settings': {
+    'react': {
+      'version': '16.13.1',
+    },
+  },
+};
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Modal = () => (<div>Hola</div>);
+const Modal = () => <div>Hola</div>;
 
 export default Modal;
