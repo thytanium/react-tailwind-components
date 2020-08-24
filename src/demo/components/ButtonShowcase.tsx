69ebf876face2686af31517d5a1c2cb81b64ab5c
Alejandro González <thytanium@gmail.com>
2020-08-24 09:30:25 -0300

    Re-sort CSS statements in demo

--- src/demo/components/ButtonShowcase.tsx
+++ src/demo/components/ButtonShowcase.tsx
@@ -15,48 +15,50 @@ export default function ButtonShowcase(): React.ReactElement {
           </Box>
         </Container>
       </div>
-      <div className="p-4 space-x-4">
-        <Button>Default</Button>
-        <Button variant={Button.variant.PRIMARY}>Primary</Button>
-        <Button variant={Button.variant.LIGHT}>Light</Button>
-        <Button variant={Button.variant.DARK}>Dark</Button>
-        <Button variant={Button.variant.SUCCESS}>Success</Button>
-        <Button variant={Button.variant.WARNING}>Warning</Button>
-        <Button variant={Button.variant.DANGER}>Danger</Button>
-      </div>
-      <div className="p-4 space-x-4">
-        <Button isDisabled>Default</Button>
-        <Button isDisabled variant={Button.variant.PRIMARY}>
-          Primary
-        </Button>
-        <Button isDisabled variant={Button.variant.LIGHT}>
-          Light
-        </Button>
-        <Button isDisabled variant={Button.variant.DARK}>
-          Dark
-        </Button>
-        <Button isDisabled variant={Button.variant.SUCCESS}>
-          Success
-        </Button>
-        <Button isDisabled variant={Button.variant.WARNING}>
-          Warning
-        </Button>
-        <Button isDisabled variant={Button.variant.DANGER}>
-          Danger
-        </Button>
-      </div>
-      <div className="p-4 space-x-4 flex">
-        <Button leftComponent={Check}>
-          <span>Default</span>
-        </Button>
-        <Button rightComponent={ChevronDown} variant={Button.variant.PRIMARY}>
-          <span>Primary</span>
-        </Button>
-        <Button leftComponent={Trash} variant={Button.variant.DANGER}>
-          <span>Danger</span>
-        </Button>
-        <Button leftComponent={RefreshCw} variant={Button.variant.WARNING} />
-      </div>
+      <Container>
+        <div className="p-4 space-x-4">
+          <Button>Default</Button>
+          <Button variant={Button.variant.PRIMARY}>Primary</Button>
+          <Button variant={Button.variant.LIGHT}>Light</Button>
+          <Button variant={Button.variant.DARK}>Dark</Button>
+          <Button variant={Button.variant.SUCCESS}>Success</Button>
+          <Button variant={Button.variant.WARNING}>Warning</Button>
+          <Button variant={Button.variant.DANGER}>Danger</Button>
+        </div>
+        <div className="p-4 space-x-4">
+          <Button isDisabled>Default</Button>
+          <Button isDisabled variant={Button.variant.PRIMARY}>
+            Primary
+          </Button>
+          <Button isDisabled variant={Button.variant.LIGHT}>
+            Light
+          </Button>
+          <Button isDisabled variant={Button.variant.DARK}>
+            Dark
+          </Button>
+          <Button isDisabled variant={Button.variant.SUCCESS}>
+            Success
+          </Button>
+          <Button isDisabled variant={Button.variant.WARNING}>
+            Warning
+          </Button>
+          <Button isDisabled variant={Button.variant.DANGER}>
+            Danger
+          </Button>
+        </div>
+        <div className="p-4 space-x-4 flex">
+          <Button leftComponent={Check}>
+            <span>Default</span>
+          </Button>
+          <Button rightComponent={ChevronDown} variant={Button.variant.PRIMARY}>
+            <span>Primary</span>
+          </Button>
+          <Button leftComponent={Trash} variant={Button.variant.DANGER}>
+            <span>Danger</span>
+          </Button>
+          <Button leftComponent={RefreshCw} variant={Button.variant.WARNING} />
+        </div>
+      </Container>
     </div>
   );
 }
