29bef6041df31c254b8196a503d5ba71b65538dd
Alejandro González <thytanium@gmail.com>
2020-08-25 20:16:53 -0300

    Rename demo css file

--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom';
 import ThemeContext from '../context/ThemeContext';
 import ButtonShowcase from './components/ButtonShowcase';
 import StackShowcase from './components/StackShowcase';
-import './rc-thytanium.css';
+import './demo.css';
 
 const App = () => (
   <ThemeContext.Provider value={{ theme: 'light' }}>
